a88819198fc9177608ee1c5ff57af0e6a2ae3a37
Rendszergazda <rendszergazda@blathy.info>
2025-01-22 11:27:51 +0100

    hoeseskesz

--- MasodikFelev/MasodikFelev/Program.cs
+++ MasodikFelev/MasodikFelev/Program.cs
@@ -12,7 +12,7 @@ namespace MasodikFelev
         const int szelXmag = 30;
         static char[,] map = new char[szelXmag, szelXmag];
         static Random r = new Random();
-        static bool moved = false;
+        //static bool moved = false;
         static void Main(string[] args)
         {
             init();
@@ -20,30 +20,48 @@ namespace MasodikFelev
             {
                 for (int i = 0; i < szelXmag; i++)
                 {
-                    if(r.Next(100) < 10)
+                    if(r.Next(101) < 10)
                     {
-                        map[0, i] = '*';
+                        map[0, i] = '❄';
                     }
                 }
-                while (!moved)
+                /*while (!moved)
                 {
                     int x = r.Next(szelXmag);
                     int y = r.Next(szelXmag);
-                    if (map[x, y] == '*')
+                    if (map[y, x] == '*' && y != 28)
                     {
                         int rx = r.Next(-1, 2);
-                        if(rx+x < 1 || rx+x > 28) { continue; }
-                        if (map[x+rx, y] != '*')
+                        if(rx+x < 1 && rx+x > 28 && x > 28) { continue; }
+                        if(x == 0 && y == 0) { continue; }
+                        if (x<29 && x > 0 && map[y + 1, x + rx] != '*')
                         {
-                            map[x, y] = ' ';
-                            map[x + rx, y] = '*';
+                            map[y, x] = ' ';
+                            map[y + 1, x + rx] = '*';
                             moved = true;
+                        }
+                    }
+                }*/
+                for (int i = szelXmag-2; i >= 0; i--)
+                {
+                    for (int j = szelXmag-1; j >= 0; j--)
+                    {
+                        if (map[i, j] == '❄' && i != 28)
+                        {
+                            int rx = r.Next(-1, 2);
+                            if (rx + j < 1 && j <= 0) { rx = r.Next(0, 2); }
+                            if (j + rx > 29 || j >= 29) { rx = r.Next(-1, 1); }
+                            if (i == 0 && j == 0) { rx = r.Next(0,2); }
+                            if (map[i + 1, j + rx] != '❄')
+                            {
+                                map[i, j] = ' ';
+                                map[i + 1, j + rx] = '❄';
+                            }
                         }
                     }
                 }
                 draw();
-                moved = false;
-                Thread.Sleep(1000);
+                Thread.Sleep(50);
             } while (true);
         }
         static void init()
@@ -57,7 +75,7 @@ namespace MasodikFelev
             }
             for (int i = 0; i < szelXmag; i++)
             {
-                map[28, i] = '*';
+                map[28, i] = '❄';
             }
         }
         static void draw()
