bf218a94391707b57277f36478b83b077229bb52
Jakab Alex Róbert <jakab.alex.robert@blathy.info>
2025-02-17 11:33:33 +0100

    szoveges

---
+++ szoveges/szoveges/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace szoveges
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> sorok = new List<string>();
+            string filepath = "a.txt";
+            //string szovegSoronkent = File.ReadAllLines(filepath);
+            //szoveMegjelen.Lines = szovegSoronkent;
+            StreamReader sr = new StreamReader(filepath);
+            string sor = sr.ReadLine();
+            while (sor != null)
+            {
+                if (sor != String.Empty) { sorok.Add(sor.Trim()); }
+                sor = sr.ReadLine();
+            }
+            sr.Close();
+            textBox2.Lines = sorok.ToArray();
+
+            string[] nev = new string[sorok.Count];
+            int[] szam = new int[sorok.Count];
+            string[] varos = new string[sorok.Count];
+
+            for (int i = 0; i < sorok.Count; i++)
+            {
+                string[] dar = sorok[i].Split(';');
+                nev[i] = dar[0];
+                szam[i] = int.Parse(dar[1]);
+                varos[i] = dar[2];
+            }
+
+            int mapDat = szam.Max();
+            int ki = Array.IndexOf(szam, mapDat);
+            string sor1 = "maxpont: "+mapDat;
+            textBox2.AppendText(sor1);
+            string sor2 = "Kivót? "+nev[ki] + varos[ki];
+            textBox2.AppendText(sor2);
+        }
+    }
+}
---
+++ szoveges/szoveges/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("szoveges")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("szoveges")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3941a5f1-caf9-4e5a-9d48-99a502baeec4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
